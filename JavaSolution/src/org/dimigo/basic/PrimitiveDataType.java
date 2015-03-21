dacd251bbbd991378f7fff51f254323632030fcc
teacher <teacher@myha>
2015-03-21 09:31:30 +0900

    실습과제 Solution

---
+++ JavaSolution/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,38 @@
+package org.dimigo.basic;
+
+/**
+ * 지금까지 배운 자료형을 사용하여 아래 내용을 출력해 보자.
+ * - 이름(String), 성별(boolean), 나이(int), 키(double), 몸무게(float), 혈액형(char)
+ *
+ * << 아이유 프로필 >>
+ * 이름 : 아이유
+ * 성별 : 여자
+ * 나이 : 23 세
+ * 키  : 161.8 cm
+ * 몸무게 : 44.3 kg
+ * 혈액형 : A 형
+ * 
+ * @author teacher
+ *
+ */
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		// 이름, 성별, 나이, 키, 몸무게, 혈액형
+		String name = "아이유";
+		boolean isMale = false;
+		int age = 23;
+		double height = 161.8;
+		float weight = 44.3f;
+		char bloodType = 'A';
+		
+		System.out.println("<< 아이유 프로필 >>");
+		System.out.println("이름 : " + name);
+		System.out.println("성별 : " + (isMale == true ? "남자" : "여자"));
+		System.out.println("나이 : " + age + " 세");
+		System.out.println("키 : " + height + " cm");
+		System.out.println("몸무게 : " + weight + " kg");
+		System.out.println("혈액형 : " + bloodType + " 형");
+	}
+
+}
---
+++ JavaSolution/src/org/dimigo/basic/Promotion.java
@@ -0,0 +1,35 @@
+package org.dimigo.basic;
+
+/**
+ * 대한민국 커피 전문점 매출1위 디미베네의 연간 인건비를 계산해보자.
+ * - 연간 인건비 = 월 평균 급여 * 12 * 점포 내 직원 수 * 점포 수
+ * - 천단위 콤마 찍기 : String.format("%,d", 10000) -> 10,000
+ *
+ * << 디미베네 연간 인건비 >>
+ * 월 평균 급여 : 1,700,000원
+ * 점포 내 직원 수 : 3명
+ * 점포 수 : 1,500개
+ * 
+ * 연간 인건비 : 91,800,000,000원
+ * 
+ * @author teacher
+ *
+ */
+public class Promotion {
+
+	public static void main(String[] args) {
+		int avgSalary = 1700000;
+		int numOfEmployee = 3;
+		int numOfStore = 1500;
+
+		System.out.println("<< 디미베네 연간 인건비 >>");
+		System.out.println("월 평균 급여 : " + String.format("%,d", avgSalary) + "원");
+		System.out.println("점포 내 직원수 : " + numOfEmployee + "명");
+		System.out.println("점포 수 : " + String.format("%,d", numOfStore) + "개");
+		
+		long annualCost = avgSalary * 12L * numOfEmployee * numOfStore;
+		
+		System.out.println("\n연간 인건비 : " + String.format("%,d", annualCost) + "원");
+		
+	}
+}
